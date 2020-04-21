7229aa20d25a343b992373536d5a02b4213ff019
amogh0503 <amoghagarwal0503@gmail.com>
2020-04-21 23:24:06 +0530

    JWT signing

--- db_assets/db_intialize.js
+++ db_assets/db_intialize.js
@@ -1,6 +1,10 @@
 const Pool = require('pg').Pool
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
+
+const secret = require('../config.js')
 const saltRounds = 10;
+
 const pool = new Pool({
     user:'postgres',
     host: 'localhost',
@@ -9,9 +13,9 @@ const pool = new Pool({
     port: '5432',
 })
 
-const getUser = (request, response) => {
+const getUser = (request, response) => {                                                      //Login
   const {email,password} = request.body
-    pool.query('SELECT password FROM users where email = $1',[email], (error, results) => {
+    pool.query('SELECT password FROM users where email = $1',[email],(error, results) => {
       user = (results.rows[0])
       if (error) {
         throw error
@@ -19,7 +23,13 @@ const getUser = (request, response) => {
       else
       { bcrypt.compare(password, user.password, function (err, results) {
         if (results == true) {
-          response.json({code:202, status:'Login Success'});  
+            const token = jwt.sign(
+            { id: email},
+            secret,
+            { expiresIn:'20d' }
+            );                                                  // JWT session upon signing
+          response.json({code:202, status:'Login Success',sign:token}); 
+          console.log(token) 
           } 
         else {
             response.json({code:203, status:'Login failure'});
@@ -43,9 +53,9 @@ const contactUser = (request, response) => {
   })
 }
 
-const getUserById = (request, response) => {
+const getUserById = (request, response) => {                                             
     const id = parseInt(request.params.id)
-  
+                                                                                        
     pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
       if (error) {
         throw error
@@ -57,16 +67,21 @@ const getUserById = (request, response) => {
   }
 
 
-const createUser = (request, response) => {
+const createUser = (request, response) => {                            // Register
     const { fname, password, phone, email, address} = request.body
     bcrypt.hash(password, saltRounds, function (err,   hash) {
     pool.query('INSERT INTO users (fname, password, phone, email, address) VALUES ($1, $2,$3,$4,$5)', [fname, hash, phone, email, address], (error, results) => {
       if (error) { 
         throw error
       }
-      else{
-        response.json({code:201,status:'created'});
-      //response.status(201).send(`User added with ID: ${results.insertId}`)
+      else{          
+           const token = jwt.sign(
+            { id: email},
+            secret,
+            { expiresIn:'20d' }
+            );
+            response.json({code:201,status:'created',sign:token});
+            console.log(token)
       }
     })
   })
